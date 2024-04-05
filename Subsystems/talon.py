7ffe4a5324a1135b50c5e33695b594777e4da42c
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2024-04-05 13:39:10 -0400

    stuff

--- Subsystems/talon.py
+++ Subsystems/talon.py
@@ -1,8 +1,8 @@
 import RPi.GPIO as GPIO
 import time
 
-class Talon:
-    """INSTANCE OF TALON MOTOR CONTROLLER"""
+class TalonSRX:
+    """INSTANCE OF TALON SRX MOTOR CONTROLLER"""
     def __init__(self, pwm_pin):
         self.pwm_pin = pwm_pin
 
@@ -12,32 +12,37 @@ class Talon:
         # Set up GPIO pin for PWM output
         GPIO.setup(self.pwm_pin, GPIO.OUT)
 
-        # Initialize PWM with a frequency of 15.625 kHz
-        self.pwm = GPIO.PWM(self.pwm_pin, 15.625)
+        # Initialize PWM with a frequency of 100 Hz (period of 10 ms)
+        self.pwm = GPIO.PWM(self.pwm_pin, 100)
         self.pwm.start(0)  # Start PWM with 0% duty cycle
 
     def set_pwm_pulse(self, pulse_width_ms):
-        # Ensure pulse width is within the range of 1-2 ms nominal, 0.67 - 2.33 ms max
-        pulse_width_ms = max(min(pulse_width_ms, 2.33), 0.67)
+        # Ensure pulse width is within the range of 1-2 ms
+        pulse_width_ms = max(min(pulse_width_ms, 2), 1)
         
         # Convert pulse width to duty cycle
-        duty_cycle = (pulse_width_ms - 1.0) / 1.0 * 100.0
+        duty_cycle = pulse_width_ms / 20 * 100
         
         # Set duty cycle for PWM signal
         self.pwm.ChangeDutyCycle(duty_cycle)
 
 if __name__ == "__main__":
     try:
-        # Initialize Talon motor controller on GPIO pin 4
-        talon = Talon(2)
+        # Initialize Talon SRX motor controller on GPIO pin 4
+        talon_srx = TalonSRX(4)
+
+        # Set PWM pulse width to 1.5 ms (neutral)
+        talon_srx.set_pwm_pulse(1.5)
+
+        time.sleep(2)  # Wait for 2 seconds
 
         # Set PWM pulse width to 1 ms (minimum throttle)
-        talon.set_pwm_pulse(1)
+        talon_srx.set_pwm_pulse(1)
 
         time.sleep(2)  # Wait for 2 seconds
 
         # Set PWM pulse width to 2 ms (maximum throttle)
-        talon.set_pwm_pulse(2)
+        talon_srx.set_pwm_pulse(2)
 
         time.sleep(2)  # Wait for 2 seconds
 
